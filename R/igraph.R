24b7c4cc48e504826bde2801abbda378852a112e
timelyportfolio <kent.russell@timelyportfolio.com>
2016-08-30 15:09:31 -0500

    add initial code for convert igraph to d3; reference #3

---
+++ R/igraph.R
@@ -0,0 +1,44 @@
+#' Convert 'igraph' to 'd3.js'
+#'
+#' @param igrf \code{igraph}
+#' @param json \code{logical} to return as \code{JSON}
+#'
+#' @return \code{list}
+#' @export
+#'
+#' @example ./inst/examples/example_igraph.R
+
+d3_igraph <- function(igrf = NULL, json=TRUE) {
+  stopifnot(!is.null(igrf), inherits(igrf,"igraph"),requireNamespace("igraph"))
+  network <- igraph::as_data_frame(
+    igrf,
+    what = "both"
+  )
+
+  # use rownames as id
+  nodes <- dplyr::mutate(network$vertices, id=rownames(network$vertices))
+  links <- dplyr::rename(
+    network$edges,
+    source = from,
+    target = to
+  )
+
+  # convert factor to character
+  nodes <- dplyr::mutate_if(nodes, is.factor, as.character)
+  links <- dplyr::mutate_if(links, is.factor, as.character)
+
+  # not necessary but if ids are numeric use zero-based indexing
+  #  and convert
+  suppressWarnings(are_ids_numeric <- !any(is.na(as.numeric(nodes$id))))
+  if(are_ids_numeric){
+    nodes$id <- as.character(as.numeric(nodes$id) - 1)
+    links$source <- as.character(as.numeric(links$source)-1)
+    links$target <- as.character(as.numeric(links$target)-1)
+  }
+
+  if(json){
+    d3_json(list(nodes=nodes,links=links),strip=FALSE)
+  } else {
+    list(nodes=nodes,links=links)
+  }
+}
---
+++ inst/examples/example_igraph.R
@@ -0,0 +1,12 @@
+\dontrun{
+library(igraph)
+library(igraphdata)
+library(d3r)
+
+# with random graph
+d3r::d3_igraph(igraph::sample_pa(100))
+
+# with karate from igraphdata
+data("karate",package="igraphdata")
+d3r::d3_igraph(karate)
+}
