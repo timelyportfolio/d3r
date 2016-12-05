timelyportfolio/d3r

commit 5787e03a6b59c89b367a88f16e9c5a899482a8d3

add colname to data; thx @yonicd for the idea; should fit nicely SearchTree

R/hierarchy.R
@@ -12,7 +12,12 @@
 change_to_id <- function(x, column=1){
   dplyr::mutate(x, children = lapply(
     quote(children),
-    function(y) dplyr::rename_(y,id=colnames(y)[column])
+    function(y) {
+      dplyr::mutate(
+        dplyr::rename_(y,id=colnames(y)[column]),
+        "colname" = colnames(y)[column]
+      )
+    }
   ))
 }
 
@@ -33,7 +38,7 @@ promote_na_one <- function(x){
   if(length(na_child_loc)){
     x <- dplyr::bind_cols(
       x,
-      dplyr::select(na_child,-(match(colnames(na_child),c("id","children"))))
+      dplyr::select(na_child,-(match(colnames(na_child),c("id","children","colname"))))
     )
 
     # eliminate na child

tests/testthat/test_hier.R
@@ -16,31 +16,37 @@ test_that("d3_nest works as json", {
   # default as json
   expect_identical(
     unclass(d3_nest(hier, value_cols="size")),
-    '{"children":[{"id":"A","children":[{"id":"A.1","children":[{"id":"A.1.a","size":2}],"size":1},{"id":"A.2","children":[{"id":"A.1.b","size":3},{"id":"A.1.c","size":4}]}]},{"id":"B","children":[{"id":"B.1","children":[{"id":"B.1.a","size":5}]},{"id":"B.2","children":[],"size":6},{"id":"B.3","children":[{"id":"B.1.b","size":7}]}]}],"id":"root"}'
+    '{"children":[{"id":"A","children":[{"id":"A.1","children":[{"id":"A.1.a","size":2,"colname":"lvl3"}],"size":1,"colname":"lvl2"},{"id":"A.2","children":[{"id":"A.1.b","size":3,"colname":"lvl3"},{"id":"A.1.c","size":4,"colname":"lvl3"}],"colname":"lvl2"}],"colname":"lvl1"},{"id":"B","children":[{"id":"B.1","children":[{"id":"B.1.a","size":5,"colname":"lvl3"}],"colname":"lvl2"},{"id":"B.2","children":[],"size":6,"colname":"lvl2"},{"id":"B.3","children":[{"id":"B.1.b","size":7,"colname":"lvl3"}],"colname":"lvl2"}],"colname":"lvl1"}],"id":"root"}'
   )
 
   # json = FALSE
   expect_identical(
     d3_nest(hier, value_cols="size", json=FALSE),
-    structure(list(children = list(structure(list(id = c("A", "B"
+structure(list(children = list(structure(list(id = c("A", "B"
 ), children = list(structure(list(id = c("A.1", "A.2"), children = list(
-    structure(list(id = "A.1.a", size = 2), .Names = c("id",
-    "size"), row.names = c(NA, -1L), class = c("tbl_df", "tbl",
-    "data.frame")), structure(list(id = c("A.1.b", "A.1.c"),
-        size = c(3, 4)), row.names = c(NA, -2L), class = c("tbl_df",
-    "tbl", "data.frame"), .Names = c("id", "size"))), size = c(1,
-NA)), row.names = c(NA, -2L), class = c("tbl_df", "tbl", "data.frame"
-), .Names = c("id", "children", "size")), structure(list(id = c("B.1",
-"B.2", "B.3"), children = list(structure(list(id = "B.1.a", size = 5), row.names = c(NA,
--1L), class = c("tbl_df", "tbl", "data.frame"), .Names = c("id",
-"size")), structure(list(id = character(0), size = numeric(0)), .Names = c("id",
-"size"), row.names = integer(0), class = c("tbl_df", "tbl", "data.frame"
-)), structure(list(id = "B.1.b", size = 7), row.names = c(NA,
--1L), class = c("tbl_df", "tbl", "data.frame"), .Names = c("id",
-"size"))), size = c(NA, 6, NA)), row.names = c(NA, -3L), class = c("tbl_df",
-"tbl", "data.frame"), .Names = c("id", "children", "size")))), row.names = c(NA,
--2L), class = c("tbl_df", "tbl", "data.frame"), .Names = c("id",
-"children"))), id = "root"), .Names = c("children", "id"), row.names = c(NA,
--1L), class = c("tbl_df", "tbl", "data.frame"))
+structure(list(id = "A.1.a", size = 2, colname = "lvl3"), .Names = c("id",
+"size", "colname"), row.names = c(NA, -1L), class = c("tbl_df",
+"tbl", "data.frame")), structure(list(id = c("A.1.b", "A.1.c"
+), size = c(3, 4), colname = c("lvl3", "lvl3")), .Names = c("id",
+"size", "colname"), class = c("tbl_df", "tbl", "data.frame"
+), row.names = c(NA, -2L))), size = c(1, NA), colname = c("lvl2",
+"lvl2")), .Names = c("id", "children", "size", "colname"), class = c("tbl_df",
+"tbl", "data.frame"), row.names = c(NA, -2L)), structure(list(
+id = c("B.1", "B.2", "B.3"), children = list(structure(list(
+id = "B.1.a", size = 5, colname = "lvl3"), .Names = c("id",
+"size", "colname"), class = c("tbl_df", "tbl", "data.frame"
+), row.names = c(NA, -1L)), structure(list(id = character(0),
+ size = numeric(0), colname = character(0)), .Names = c("id",
+"size", "colname"), row.names = integer(0), class = c("tbl_df",
+"tbl", "data.frame")), structure(list(id = "B.1.b", size = 7,
+colname = "lvl3"), .Names = c("id", "size", "colname"
+), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA,
+-1L))), size = c(NA, 6, NA), colname = c("lvl2", "lvl2",
+"lvl2")), .Names = c("id", "children", "size", "colname"), class = c("tbl_df",
+"tbl", "data.frame"), row.names = c(NA, -3L))), colname = c("lvl1",
+"lvl1")), .Names = c("id", "children", "colname"), class = c("tbl_df",
+"tbl", "data.frame"), row.names = c(NA, -2L))), id = "root"), .Names = c("children",
+"id"), row.names = c(NA, -1L), class = c("tbl_df", "tbl", "data.frame"
+))
   )
 })

commit ddc94dc3ce488c2df358ecaf0548410300b4e8ae

fix bug introduced in 5787e03a6b59c89b367a88f16e9c5a899482a8d3 and change "id" to "name"; closes #10

R/hierarchy.R
@@ -1,20 +1,16 @@
-### Work In Progress function
-###  to create nested d3 hierarchy
-### will make s3 with some iteration
-
-#' Change Column Name of Children to "id"
+#' Change Column Name in Children to "name"
 #'
 #' @param x \code{data.frame} or \code{data.frame} derivative, such
 #'         as \code{tibble}
 #' @param column column to convert
 #'
 #' @return \code{data.frame}
-change_to_id <- function(x, column=1){
+change_to_name <- function(x, column=1){
   dplyr::mutate(x, children = lapply(
     quote(children),
     function(y) {
       dplyr::mutate(
-        dplyr::rename_(y,id=colnames(y)[column]),
+        dplyr::rename_(y,name=colnames(y)[column]),
         "colname" = colnames(y)[column]
       )
     }
@@ -31,14 +27,14 @@ promote_na_one <- function(x){
   # find children that are na
   # expect this to only happen once, so only take first na
   #   to define values
-  na_child <- dplyr::filter(x$children[[1]], is.na(quote(id)))[1,]
-  na_child_loc <- which(is.na(x$children[[1]]$id))
+  na_child <- dplyr::filter(x$children[[1]], is.na(quote(name)))[1,]
+  na_child_loc <- which(is.na(x$children[[1]]$name))
 
-  # promote all non-id columns to top level
+  # promote all non-d3r columns to top level
   if(length(na_child_loc)){
     x <- dplyr::bind_cols(
       x,
-      dplyr::select(na_child,-(match(colnames(na_child),c("id","children","colname"))))
+      dplyr::select(na_child,-(match(colnames(na_child),c("name","children","colname"))))
     )
 
     # eliminate na child
@@ -90,7 +86,7 @@ d3_nest <- function(
   data <- dplyr::mutate_if(data, is.factor, as.character)
 
   data_nested <- dplyr::bind_rows(promote_na(
-    change_to_id(
+    change_to_name(
       tidyr::nest_(
         data=data,
         nest_cols=c(nonnest_cols[length(nonnest_cols)], value_cols),
@@ -101,11 +97,11 @@ d3_nest <- function(
 
   for(x in rev(
     colnames(data_nested)[
-      -which(colnames(data_nested) %in% c("children",value_cols))
+      -which(colnames(data_nested) %in% c("children","colname",value_cols))
     ]
   )){
     data_nested <- dplyr::bind_rows(promote_na(
-      change_to_id(
+      change_to_name(
         tidyr::nest_(
           data_nested,
           nest_cols = colnames(data_nested)[colnames(data_nested) %in% c(x,"children",value_cols)],
@@ -114,7 +110,7 @@ d3_nest <- function(
       )
     ))
   }
-  data_nested$id = root
+  data_nested$name = root
   if(json){
     d3_json(data_nested,strip=TRUE)
   } else {

man/change_to_name.Rd
@@ -1,10 +1,10 @@
 % Generated by roxygen2: do not edit by hand
 % Please edit documentation in R/hierarchy.R
-\name{change_to_id}
-\alias{change_to_id}
-\title{Change Column Name of Children to "id"}
+\name{change_to_name}
+\alias{change_to_name}
+\title{Change Column Name in Children to "name"}
 \usage{
-change_to_id(x, column = 1)
+change_to_name(x, column = 1)
 }
 \arguments{
 \item{x}{\code{data.frame} or \code{data.frame} derivative, such
@@ -16,6 +16,6 @@ as \code{tibble}}
 \code{data.frame}
 }
 \description{
-Change Column Name of Children to "id"
+Change Column Name in Children to "name"
 }
 