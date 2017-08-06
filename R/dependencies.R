06bbeb667305be21bc89834e418785a611527bdf
beemyfriend <plzbeemyfriend@gmail.com>
2017-08-06 13:52:24 -0400

    think I figured out the travis-ci issue

--- R/dependencies.R
+++ R/dependencies.R
@@ -50,16 +50,17 @@ d3_dep_v3 <- function(offline=TRUE){
 }
 
 #' 'd3.js' Dependency for Version 4 Jetpack
-#' 
+#'
 #' d3-jetpack is a set of nifty convenience wrappers that speed up your daily work with d3.js. Must be included after `d3_dep_v4()`
 #' Learn more by reading https://github.com/gka/d3-jetpack or by watching https://www.youtube.com/watch?v=_5ky0AYq_Dg&t=4s
-#' 
-#' @param there are none
+#'
+#' @param offline \code{logical} to specify whether to use
+#'         a local copy of d3.js (\code{TRUE}) or use cdn (\code{FALSE})
 #' @return \code{htmltools::htmlDependency}
 #' @export
 #' @examples
 #'
-#' 
+#'
 #' library(d3r)
 #' library(htmltools)
 #'
@@ -84,16 +85,16 @@ d3_dep_v3 <- function(offline=TRUE){
   #' "
 #' ))), d3_dep_v4(), d3_dep_jetpack())
 #' browsable(t)
-#' 
 #'
-#' 
+#'
+#'
 #' t <- tagList(tags$script(HTML(sprintf(
 #'  "
 #'  var svg = d3.select('body')
 #'      .append('svg');
-#'  
+#'
 #'  test_data = [{x: 250, y: 250}, {x: 300, y: 300}, {x: 300, y: 100}];
-#'  
+#'
 #'  svg.appendMany(test_data, 'circle')
 #'      .at({
 #'          cx: function(d){return d.x},
@@ -107,15 +108,15 @@ d3_dep_v3 <- function(offline=TRUE){
 #'  "
 #' ))), d3_dep_v4(), d3_dep_jetpack())
 #' browsable(t)
-#' 
+#'
 d3_dep_jetpack <- function(offline=TRUE){
-  
+
   if(offline) {
     src <- c(file=system.file("www/d3/d3-jetpack/dist", package="d3r"))
   } else {
     src <- c(href="https://unpkg.com/d3-jetpack@2.0.7/build/")
   }
-  
+
 htmltools::htmlDependency(
     name = "d3-jetpack",
     version = "2.0.7",
