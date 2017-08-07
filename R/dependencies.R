34e0851b7db9245cb404b04fa29b2a8ef309fc40
timelyportfolio <kent.russell@timelyportfolio.com>
2017-08-07 05:58:21 -0500

    update d3v4, make small changes to jetpack roxygen

--- R/dependencies.R
+++ R/dependencies.R
@@ -18,7 +18,7 @@ d3_dep_v4 <- function(offline=TRUE){
 
   htmltools::htmlDependency(
     name = "d3",
-    version = "4.9.1",
+    version = "4.10.0",
     src = src,
     script = "d3.min.js"
   )
@@ -51,63 +51,16 @@ d3_dep_v3 <- function(offline=TRUE){
 
 #' 'd3.js' Dependency for Version 4 Jetpack
 #'
-#' d3-jetpack is a set of nifty convenience wrappers that speed up your daily work with d3.js. Must be included after `d3_dep_v4()`
-#' Learn more by reading https://github.com/gka/d3-jetpack or by watching https://www.youtube.com/watch?v=_5ky0AYq_Dg&t=4s
+#' d3-jetpack is a set of nifty convenience wrappers that speed up
+#' your daily work with d3.js. Must be included after \code{d3_dep_v4()}.
+#' Learn more by reading \href{https://github.com/gka/d3-jetpack}{d3-jetpack}
+#' or by watching this \href{https://www.youtube.com/watch?v=_5ky0AYq_Dg&t=4s}{YouTube}.
 #'
 #' @param offline \code{logical} to specify whether to use
 #'         a local copy of d3.js (\code{TRUE}) or use cdn (\code{FALSE})
 #' @return \code{htmltools::htmlDependency}
 #' @export
-#' @examples
-#'
-#'
-#' library(d3r)
-#' library(htmltools)
-#'
-#' t <- tagList(tags$script(HTML(sprintf(
-#' "
-#' var x = 5;
-#'
-#' var svg = d3.select('body')
-#'     .append('svg');
-#'
-#' svg.append('rect')
-#'     .at({
-  #'         x: 100,
-  #'         y: 100,
-  #'         width: 100,
-  #'         height: 100
-  #'     })
-#'     .st({
-  #'         fill: 'blue',
-  #'         stroke: 'purple'
-  #'     });
-  #' "
-#' ))), d3_dep_v4(), d3_dep_jetpack())
-#' browsable(t)
-#'
-#'
-#'
-#' t <- tagList(tags$script(HTML(sprintf(
-#'  "
-#'  var svg = d3.select('body')
-#'      .append('svg');
-#'
-#'  test_data = [{x: 250, y: 250}, {x: 300, y: 300}, {x: 300, y: 100}];
-#'
-#'  svg.appendMany(test_data, 'circle')
-#'      .at({
-#'          cx: function(d){return d.x},
-#'          cy: function(d){return d.y},
-#'          r: 50
-#'      })
-#'      .st({
-#'          fill: 'purple',
-#'          stroke: 'blue'
-#'      });
-#'  "
-#' ))), d3_dep_v4(), d3_dep_jetpack())
-#' browsable(t)
+#' @example ./inst/examples/example_d3_jetpack.R
 #'
 d3_dep_jetpack <- function(offline=TRUE){
 
