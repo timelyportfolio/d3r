b49e70930b35921f513ccc2f92b44593d64d9a2c
timelyportfolio <kent.russell@timelyportfolio.com>
2020-05-25 08:33:00 -0500

    update d3_v8 to use d3_dep_v5

--- tests/testthat/test_v8.R
+++ tests/testthat/test_v8.R
@@ -8,7 +8,7 @@ test_that("d3_v8 has d3", {
   # d3_v8 works returns v8 context with d3 installed
   expect_identical(
     d3_v8()$get("global.d3.version"),
-    d3_dep_v4()$version
+    d3_dep_v5()$version
   )
 
 })
